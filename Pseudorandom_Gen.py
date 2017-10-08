5c90b8db1f462ec9ac75fc084e3f8223c51f9e17
absanyal <absanyal.ABS@gmail.com>
2017-10-08 06:37:32 +0530

    Beautified

--- Pseudorandom_Gen.py
+++ Pseudorandom_Gen.py
@@ -1,20 +1,26 @@
 import matplotlib.pyplot as plt
 
-#XOR function
+# XOR function
+
+
 def XOR(a, b):
     if (a + b == 1):
         return 1
     else:
         return 0
 
-#OR function
+# OR function
+
+
 def OR(a, b):
     if (a + b == 0):
         return 0
     else:
         return 1
 
-#NOT function
+# NOT function
+
+
 def NOT(a):
     if a == 0:
         p = 1
@@ -22,16 +28,20 @@ def NOT(a):
         p = 0
     return p
 
-#Shift Right function
+# Shift Right function
+
+
 def ShiftR(a):
     i = len(a) - 1
-    while ( i > 0):
+    while (i > 0):
         a[i] = a[i - 1]
         i -= 1
     a[0] = 0
     return a
 
-#Bin To Dec Function
+# Bin To Dec Function
+
+
 def BinToDec(bin_list):
     dec = 0
     p = 0
@@ -41,6 +51,7 @@ def BinToDec(bin_list):
         p += 1
         i -= 1
     return dec
+
 
 i = 0
 x = []
@@ -53,10 +64,10 @@ randlist = []
 stop_val = 50
 i = 0
 while (i < stop_val):
-    #randlist.append(BinToDec(x))
+    # randlist.append(BinToDec(x))
     print(x, BinToDec(x))
     x = ShiftR(x)
-    x[0] = NOT ( XOR( x[len(x) - 1], x[len(x) - 2] ) )
+    x[0] = NOT(XOR(x[len(x) - 1], x[len(x) - 2]))
     i += 1
 
-#plt.hist(randlist, bins = 10, normed = True)
+plt.hist(randlist, bins=10, normed=True)
